de43b7ad27183f02d98d9d6466f6b43991ca2272
iga-yamaguchi <iganin2@gmail.com>
2018-02-02 15:57:36 +0900

    Display article list in home.

--- AspNetSampleBlog/Controllers/HomeController.cs
+++ AspNetSampleBlog/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AspNetSampleBlog.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,10 @@ namespace AspNetSampleBlog.Controllers
 {
     public class HomeController : Controller
     {
+        private MvcBasicContext db = new MvcBasicContext();
         public ActionResult Index()
         {
-            return View();
+            return View(db.Articles.ToList());
         }
 
         public ActionResult About()
